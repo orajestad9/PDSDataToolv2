3362002835471c755dd4c2a9658d0827cf9a2da4
orajestad9 <robflot87@gmail.com>
2016-09-27 12:23:10 -0500

    Add files via upload

---
+++ PDSDataTool/userReq.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDSDataToolv2
+{
+    class userReq
+    {
+        public void getUserEntry()
+        {
+            Console.WriteLine("Please select ");
+        }
+        public void setListingReqs()
+        {
+
+        }
+    }
+}
