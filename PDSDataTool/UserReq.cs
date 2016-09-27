3ccb66cf80ccfce671bde72287ecbcfd47b4fc67
orajestad9 <robflot87@gmail.com>
2016-09-27 15:46:57 -0500

    add files

---
+++ PDSDataTool/UserReq.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDSDataTool
+{
+    class UserReq
+    {
+        public void getUserEntry()
+        {
+            Console.WriteLine("Please select a combination from the list below:");
+            var dataCombo = Console.ReadLine();
+        };
+    }
+}
